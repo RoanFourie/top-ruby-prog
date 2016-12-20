aa9cf0ddd8417007c372b33b04d3cdb9246594e1
Roan Fourie <roanfourie@gmail.com>
2016-12-20 09:35:06 +0200

    Completed stock picker project

--- stock_picker.rb
+++ stock_picker.rb
@@ -6,16 +6,16 @@ def stock_picker(stock_prices)
     largest_profit = 0
     profit = 0
     (0..(stock_prices.length-2)).each do |a| # buy (skip last day)
-            (1..(stock_prices.length-1)).each do |b| # sell (skip first day)
-                if a < b # You cannot sell before you buy
-                    profit = stock_prices[b] - stock_prices[a]
-                    if profit > largest_profit
-                        largest_profit = profit
-                        day_buy = a
-                        day_sell = b
-                    end
+        (1..(stock_prices.length-1)).each do |b| # sell (skip first day)
+            if a < b # You cannot sell before you buy
+                profit = stock_prices[b] - stock_prices[a]
+                if profit > largest_profit
+                    largest_profit = profit
+                    day_buy = a
+                    day_sell = b
                 end
             end
+        end
     end
     result = [day_buy, day_sell]
     return result
